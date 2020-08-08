e2522ecd1cf1b731ddae501eb5bee87c2dc8b23f
jadson76 <biojadson@gmail.com>
2020-08-08 09:00:53 -0300

    Teste com banco H2

--- src/main/java/com/jadson/cursomc/domain/Categoria.java
+++ src/main/java/com/jadson/cursomc/domain/Categoria.java
@@ -2,9 +2,17 @@ package com.jadson.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{	
 	private static final long serialVersionUID = 4716407773538609482L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String nome;
