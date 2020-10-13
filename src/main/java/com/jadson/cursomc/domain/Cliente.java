253b14166578778e5ca171e14a9d7f60ac539e8e
jadson76 <biojadson@gmail.com>
2020-10-12 21:50:02 -0300

    models venda

--- src/main/java/com/jadson/cursomc/domain/Cliente.java
+++ src/main/java/com/jadson/cursomc/domain/Cliente.java
@@ -43,6 +43,8 @@ public class Cliente implements Serializable{
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	private List<Pedido> pedidos = new ArrayList<Pedido>();
+	
 	
 	public Cliente() {
 		
@@ -116,6 +118,16 @@ public class Cliente implements Serializable{
 
 	public void setTipo(Integer tipo) {
 		this.tipo = tipo;
+	}
+	
+	
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
---
+++ src/main/java/com/jadson/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,36 @@
+package com.jadson.cursomc.domain;
+
+import java.io.Serializable;
+
+import com.jadson.cursomc.domain.enums.EstadoPagamento;
+
+public class PagamentoComCartao extends Pagamento implements Serializable{
+
+
+	private static final long serialVersionUID = -6253501013584093332L;
+	
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {
+		
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.numeroDeParcelas = numeroDeParcelas;	
+		
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+	
+	
+	
+	
+
+}
